8a28534befeba371d07246084eb30fbaa8955d1a
Mark Anthony Vale <markavale1@gmail.com>
2021-03-14 01:53:10 +0800

    add scraper model architecture

--- scraper/admin.py
+++ scraper/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from . models import Scraper, Spider, SpiderUrls
+from . models import Scraper, Spider, SpiderCrawler
 
 admin.site.register(Scraper)
 admin.site.register(Spider)
-admin.site.register(SpiderUrls)
+admin.site.register(SpiderCrawler)
--- scraper/models.py
+++ scraper/models.py
@@ -1,28 +1,66 @@
 from django.db import models
+from django.conf import settings
 
-# from django.conf import settings
+User = settings.AUTH_USER_MODEL
 
 class Scraper(models.Model):
-    spiders = models.ManyToManyField('Spider')
-    is_finish = models.BooleanField(default=False)
-    created = models.DateTimeField(auto_now_add=True)
+    user        = models.ForeignKey(User, on_delete=models.CASCADE)
+    data        = models.IntegerField(default=0)
+    workers     = models.IntegerField(default=0)
+    spiders     = models.ManyToManyField('Spider')
+    info_log    = models.TextField(blank=True, null=True)
+    error_log   = models.TextField(blank=True, null=True)
+    cron_log    = models.TextField(blank=True, null=True)
+    is_finish   = models.BooleanField(default=False)
+    created     = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.created + " " + self.spiders.count()
+        return self.user.username
 
-    def get_total_scraper_errors(self):
+    def total_errors(self):
         total = 0
-        for spider in self.spiders.all:
-            total += spider.get_total_errors 
+        for spider in self.spiders.all():
+            total += spider.get_total_scraper_errors() 
+        return total
+    
+    def total_skip(self):
+        total = 0
+        for skip in self.spiders.all():
+            total += spder.get_total_skip()
         return total
 
 class Spider(models.Model):
-    spider_urls     = models.ManyToManyField('SpiderUrls')
-    http_error      = models.IntegerField(default=0)
-    dns_error       = models.IntegerField(default=0)
-    timeout_error   = models.IntegerField(default=0)
-    base_error      = models.IntegerField(default=0)
-    is_finish       = models.BooleanField(default=False)
+    crawlers            = models.ManyToManyField('SpiderCrawler')
+
+
+    def get_total_scraper_errors(self):
+        total = 0
+        for spider in self.crawlers.all():
+            total += spider.get_total_errors() 
+        return total
+
+    def get_total_skip(self):
+        total = 0
+        for skip in self.crawlers.all():
+            total += spder.skip_url
+        return total
+
+    def get_avg_download_latency(self):
+        dl_latency = 0
+        for latency in self.crawlers.all():
+            dl_latency +=latency.download_latency
+        avg = dl_latency / self.crawlers.count()
+        return avg
+
+class SpiderCrawler(models.Model):
+    spider_data         = models.IntegerField(default=0)#ManyToManyField('SpiderUrls')
+    download_latency    = models.FloatField()
+    http_error          = models.IntegerField(default=0)
+    dns_error           = models.IntegerField(default=0)
+    timeout_error       = models.IntegerField(default=0)
+    base_error          = models.IntegerField(default=0)
+    skip_url            = models.IntegerField(default=0)
+    is_finish           = models.BooleanField(default=False)
 
     def __str__(self):
         return "Spider"
@@ -32,11 +70,11 @@ class Spider(models.Model):
     
 
 
-class SpiderUrls(models.Model):
-    url = models.URLField()
+# class SpiderUrls(models.Model):
+#     url = models.URLField()
 
-    class Meta:
-        verbose_name_plural = "Spider URLS"
+#     class Meta:
+#         verbose_name_plural = "Spider URLS"
 
-    def __str__(self):
-        return self.url
+#     def __str__(self):
+#         return self.url
